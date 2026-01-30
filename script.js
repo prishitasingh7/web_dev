6980539b99616cac2d825d1d1735fcb048362864
prishita singh <prishitasingh@prishitas-MacBook-Air.local>
2026-01-30 13:49:18 +0530

    code

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputElem = document.querySelector("input");
+const buttonElem = document.querySelector("#search");
+
+buttonElem.addEventListener("click", ()=> {
+    const location = inputElem.value.trim();
+
+    
+
+    fn9
+
+
+    
+fn90
+
+    inputElem.value="";
+})
